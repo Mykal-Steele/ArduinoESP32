89c50284d961e2d9ed6fe0d65b2ea2f7378d8455
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-01-04 12:24:22 +0700

    Create backandforth.ino

---
+++ Sequential_blinking/backandforth/backandforth.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+
+  digitalWrite(18, LOW);
+  digitalWrite(19, LOW);
+}
+
+void loop() {
+  digitalWrite(18, HIGH);
+  digitalWrite(19, LOW);
+  delay(500);
+  digitalWrite(18, LOW);
+  digitalWrite(19, HIGH);
+  delay(500);
+
+}
